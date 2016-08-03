ec855f00d04affefd40e87383885603774ccfd64
Adrian DC <radian.dc@gmail.com>
2016-08-03 21:13:34 +0530

    hal: Allow msm8960 to use msm8974 audio HAL
    Change-Id: If13c31eec09e3e684adf362c15575bbcd44df420
    Signed-off-by: Adrian DC <radian.dc@gmail.com>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- hal/Android.mk
+++ hal/Android.mk
@@ -11,7 +11,7 @@ ifneq ($(filter msm8960,$(TARGET_BOARD_PLATFORM)),)
   LOCAL_CFLAGS += -DMAX_TARGET_SPECIFIC_CHANNEL_CNT="2"
   LOCAL_CFLAGS += -DNO_CAPTURE_FM_MODE
 endif
-ifneq ($(filter msm8974 msm8226 msm8084 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8974 msm8226 msm8084 msm8992 msm8994 msm8996 msm8998,$(TARGET_BOARD_PLATFORM)),)
   # B-family platform uses msm8974 code base
   AUDIO_PLATFORM = msm8974
 ifneq ($(filter msm8974,$(TARGET_BOARD_PLATFORM)),)
